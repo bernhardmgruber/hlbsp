a9b5f7859f481ed46805173415ae81332c731bad
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2018-08-15 01:34:58 +0200

    improved opengl debug callback

--- src/GLRenderer.cpp
+++ src/GLRenderer.cpp
@@ -17,6 +17,43 @@ namespace {
 
 	constexpr auto CONSOLE_WIDTH = 400;
 	constexpr auto CONSOLE_HEIGHT = 300;
+
+	void debugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
+		const char* sourceStr = [&] {
+			switch (source) {
+			case GL_DEBUG_SOURCE_API_ARB:             return "API";
+			case GL_DEBUG_SOURCE_SHADER_COMPILER_ARB: return "shader compiler";
+			case GL_DEBUG_SOURCE_WINDOW_SYSTEM_ARB:   return "window system";
+			case GL_DEBUG_SOURCE_THIRD_PARTY_ARB:     return "third party";
+			case GL_DEBUG_SOURCE_APPLICATION_ARB:     return "application";
+			case GL_DEBUG_SOURCE_OTHER_ARB:           return "other";
+			default:                                  return "unknown";
+			}
+		}();
+
+		const char* typeStr = [&] {
+			switch (type) {
+				case GL_DEBUG_TYPE_ERROR_ARB:               return "error";
+				case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR_ARB: return "deprecated behavior";
+				case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR_ARB : return "undefined behavior";
+				case GL_DEBUG_TYPE_PERFORMANCE_ARB:         return "performance";
+				case GL_DEBUG_TYPE_PORTABILITY_ARB:         return "portability";
+				case GL_DEBUG_TYPE_OTHER_ARB:               return "other";
+				default:                                    return "unknown";
+			}
+		}();
+
+		const char* severityStr = [&] {
+			switch (severity) {
+				case GL_DEBUG_SEVERITY_HIGH_ARB:   return "high";
+				case GL_DEBUG_SEVERITY_MEDIUM_ARB: return "medium";
+				case GL_DEBUG_SEVERITY_LOW_ARB:    return "low";
+				default:                           return "unknown";
+			}
+		}();
+
+		std::cerr << "OpenGL debug callback: [" << severityStr << "|" << sourceStr << "|" << typeStr << "] " << std::string(message, length) << '\n';
+	}
 }
 
 GLRenderer::GLRenderer() {
@@ -25,10 +62,8 @@ GLRenderer::GLRenderer() {
 
 	// error callback
 	if (GLEW_ARB_debug_output) {
-		glDebugMessageCallbackARB([](GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
-			std::cerr << "OpenGL debug callback: " << std::string(message, length);
-		},
-			nullptr);
+		glDebugMessageCallbackARB(debugCallback, nullptr);
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
 	}
 
 	std::clog << "Setting rendering states ...\n";
