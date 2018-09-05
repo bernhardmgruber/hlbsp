f00474e49c58966aab09bfe9d613aa60043e9d66
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2018-09-06 00:43:14 +0200

    provided debugCallback overload for older GLEW versions

--- src/GLRenderer.cpp
+++ src/GLRenderer.cpp
@@ -47,13 +47,18 @@ namespace {
 			__debugbreak();
 #endif
 	}
+
+	// some GLEW versions still have the last parameter mutable (looking at you travis)
+	void debugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, void* userParam) {
+		debugCallback(source, type, id, severity, length, message, userParam);
+	}
 }
 
 GLRenderer::GLRenderer() {
 #ifndef NDEBUG
 	// error callback
 	if (GLEW_ARB_debug_output) {
-		glDebugMessageCallback(debugCallback, nullptr);
+		glDebugMessageCallback(static_cast<GLDEBUGPROC>(debugCallback), nullptr);
 		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
 	}
 #endif
